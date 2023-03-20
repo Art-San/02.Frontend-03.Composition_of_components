8933453fa984e45289bdae9775157f14b29d20cb
Aleksander <asava@sibmail.com>
2023-03-20 22:15:59 +0700

    Рефакторинг кода

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 
 const Counter = () => {
 	const [count, setCount] = useState(0)
-	const [tags, setTegs] = useState(['tag1', 'teg2', 'teg3'])
 
 	const formatCount = () => {
 		return count === 0 ? 'empty' : count
@@ -20,62 +19,6 @@ const Counter = () => {
 
 	const handleDecrement = () => {
 		setCount(prevState => prevState - 1)
-	}
-
-	const handleTagChange = id => {
-		setTegs(prevState => prevState.filter(t => t !== id))
-	}
-
-	// ПЕРВЫЙ ВАРИАНТ
-
-	// const renderTags = () => {
-	// 	if (tags.length === 0) return 'No Tegs'
-	// 	return tags.map((tag, i) => (
-	// 		<li
-	// 			key={i}
-	// 			className='btn btn-primary btn-sm m-2'
-	// 			onClick={() => handleTagChange(tag)}
-	// 		>
-	// 			{tag}
-	// 		</li>
-	// 	))
-	// }
-
-	// ВТОРОЙ ВАРИАНТ
-
-	// const renderTags = () => {
-	// 	return tags.length !== 0
-	// 		? tags.map((tag, i) => (
-	// 				<li
-	// 					key={i}
-	// 					className='btn btn-primary btn-sm m-2'
-	// 					onClick={() => handleTagChange(tag)}
-	// 				>
-	// 					{tag}
-	// 				</li>
-	// 		  ))
-	// 		: 'No Tegs'
-	// }
-
-	// ТРЕТИЙ ВАРИАНТ
-
-	const renderTags = () => {
-		return (
-			tags.length !== 0 &&
-			tags.map((tag, i) => (
-				<li
-					key={i}
-					className='btn btn-primary btn-sm m-2'
-					onClick={() => handleTagChange(tag)}
-				>
-					{tag}
-				</li>
-			))
-		)
-	}
-	// Четвертый вариант одно или другое
-	if (tags.length !== 0) {
-		return <ul>{renderTags()}</ul>
 	}
 
 	return (
